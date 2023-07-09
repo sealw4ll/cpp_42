2e757126391aa6070b7c9cff6fac0a9d9e6452e3
Wallace Ting <61623069+sealw4ll@users.noreply.github.com>
2023-07-09 13:16:44 +0000

    ex00 done TODO: learn classes and implement for phonebook

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+	class Contact
+	{
+		public:
+			get_contact(int index);
+	}
+
+	class PhoneBook
+	{
+		public:
+			get_contact();
+		private:
+			[8]
+	}
+	#include <iostream>
+
+	using std::cout;
+	using std::string;
+	using std::endl;
+
+#endif
